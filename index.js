9f5210d3d780a7dec4950266134e8521f1014c4d
Vlad Feskov <v@vladimirfeskov.com>
2019-08-13 17:25:07 +0200

    avoid checking deadends multiple times

--- index.js
+++ index.js
@@ -26,65 +26,108 @@ const cube = {
   z: 5
 }
 
+const ns = Math.max(cube.x, cube.y, cube.z);
+const k = [ns**5, ns**4, ns**3, ns**2, ns, 1];
+
 const T = 'Time passed'
-let deadends = 0;
+const deadends = {};
+let deadendsCount = 0;
 
 console.log(`Start timestamp: ${Date.now()}\n`);
 console.time(T);
-const result = putNextPiece([], [...allPieces])
-if (result) {
-  console.log(JSON.stringify(result, null, 2));
+const placements = putNextPiece([], [...allPieces])
+if (placements) {
+  console.log(`SOLUTION: ${JSON.stringify(placements, null, 2)}`);
 } else {
-  console.log('No solution found');
+  console.log('NO SOLUTION FOUND :\'(');
 }
 console.timeEnd(T);
-console.log(`Total dead ends: ${deadends}\n`);
+console.log(`Total dead ends: ${deadendsCount}`);
+console.log(`Total unique ends: ${Object.keys(deadends).length}`);
 console.log(`End timestamp: ${Date.now()}`);
 
-function putNextPiece(occupiedPoints, pieces, result = []) {
+function putNextPiece(occupiedPoints, pieces, placements = []) {
   if (pieces.length === 0) {
-    return beautifyResult(result);
+    return decodePlacements(placements);
   }
-  const otherPieces = [...pieces];
-  const piece = otherPieces.pop();
-  for (let variant of pieceTypes[piece].variants) {
-    for (let x = 0; x < cube.x - variant.x + 1; x++) {
-      for (let y = 0; y < cube.y - variant.y + 1; y++) {
-        for (let z = 0; z < cube.z - variant.z + 1; z++) {
-          const points = getPoints(variant, { x, y, z });
-          const cantFit = points.some(point => occupiedPoints.includes(point));
-          if (cantFit) {
-            continue;
-          }
-          const newOccupiedPoints = [...occupiedPoints, ...points];
-          const finalResult = putNextPiece(
-            newOccupiedPoints,
-            otherPieces,
-            [...result, {
-              piece,
-              coords: { x, y, z },
-              variant
-            }]
-          );
-          if (finalResult) {
-            return finalResult;
+  const placementsKey = getPlacementsKey(placements);
+  const placementsAreDeadend = typeof deadends[placementsKey] !== 'undefined';
+  if (!placementsAreDeadend) {
+    const otherPieces = [...pieces];
+    const piece = otherPieces.pop();
+    const variants = pieceTypes[piece].variants;
+    for (let vi = variants.length - 1; vi >= 0; vi--) {
+      const variant = variants[vi];
+      for (let x = cube.x - variant.x; x >= 0; x--) {
+        for (let y = cube.y - variant.y; y >= 0; y--) {
+          for (let z = cube.z - variant.z; z >= 0; z--) {
+            const points = getPoints(variant, { x, y, z });
+            const cantFit = points.some(point => occupiedPoints.includes(point));
+            if (cantFit) {
+              continue;
+            }
+            const newOccupiedPoints = [...occupiedPoints, ...points];
+            const finalPlacements = putNextPiece(
+              newOccupiedPoints,
+              otherPieces,
+              [...placements, encodePiecePlacement({ variant, coords: { x, y, z } })]
+            );
+            if (finalPlacements) {
+              return finalPlacements;
+            }
           }
         }
       }
     }
+    deadends[placementsKey] = null;
   }
-  if(++deadends % 10**4 === 0) {
+  if(++deadendsCount % 10**5 === 0) {
     console.timeLog(T);
-    console.log(`Latest dead end: ${JSON.stringify(beautifyResult(result))}`)
-    console.log(`Dead ends so far: ${deadends / 10**6} million\n`);
+    console.log(`Latest dead end: ${JSON.stringify(decodePlacements(placements))}`)
+    console.log(`Dead ends so far: ${deadendsCount / 10**6} million`);
+    console.log(`Unique dead ends so far: ${Object.keys(deadends).length}\n`)
     global.gc();
   }
 }
 
-function beautifyResult(result) {
-  const r =  result.map(i => {
-    i.name = pieceTypes[i.piece].name;
-    return i;
+function decodePlacements(placements) {
+  return placements.map(code => {
+    const placement = decodePiecePlacement(code);
+    const pv = placement.variant;
+    const pieceType = [Big, Tiny, Flat].find(
+      ({ variants }) => variants.some(v => v.x === pv.x && v.y === pv.y && v.z === pv.z)
+    );
+    if (pieceType) {
+      placement.name = pieceType.name;
+    }
+    return placement;
   });
-  return r;
+}
+
+function encodePiecePlacement({ variant, coords }) {
+  return k[0] * variant.x +
+         k[1] * variant.y +
+         k[2] * variant.z +
+         k[3] * coords.x +
+         k[4] * coords.y +
+         k[5] * coords.z;
+}
+
+function decodePiecePlacement(code) {
+  return {
+    variant: {
+      x: Math.floor(code / k[0]),
+      y: Math.floor(code / k[1]) % ns,
+      z: Math.floor(code / k[2]) % ns
+    },
+    coords: {
+      x: Math.floor(code / k[3]) % ns,
+      y: Math.floor(code / k[4]) % ns,
+      z: code % ns
+    }
+  };
+}
+
+function getPlacementsKey(placements) {
+  return placements.sort((a, b) => a - b).join('-');
 }
